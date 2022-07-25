93824c22bb7f942e254b71f6951faf5ccdfabb2a
Tanmoy <tanmoykhatua97@gmail.com>
2022-07-25 20:31:08 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "Select" };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+
+      <form style={{ color: "red", padding: '20px', marginLeft: '35%', marginTop: '10%', fontSize: '2rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', backgroundColor: '#ccc', width: '450px' }} onSubmit={this.handleSubmit}>
+        <label style={{ margin: '20px' }}>
+          Should you use a Dropdown
+        </label>
+        <select defaultValue={"Select"} style={{ width: '200px', marginLeft: '25%', height: '30px', borderRadius: '20px', marginBottom: '30px', textAlign: 'center' }} value={this.state.value} onChange={this.handleChange}>
+          <option value="Select">Select</option>
+          <option value="YES">YES</option>
+          <option value="NO">NO</option>
+          <option value="Probably not">Probably not</option>
+
+        </select>
+        <input style={{ width: '100px', marginLeft: '35%', height: '30px', borderRadius: '20px', marginBottom: '30px' }} type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
